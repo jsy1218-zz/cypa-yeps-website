ed54bd8ac61688e6b446d134d41c1075ade71b62
Siyu Jiang <blackjackjiang@gmail.com>
2018-10-15 01:33:32 -0700

    1. NavBar implementation with css integration issue 2. embed the static img file for demo

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,12 @@
 import * as React from 'react';
 import './App.css';
-
-import logo from './logo.svg';
+const imgSource = require('src/img/mockup.png');
 
 class App extends React.Component {
   public render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.tsx</code> and save to reload.
-        </p>
+         <img src={String(imgSource)} alt="Mockup"/>
       </div>
     );
   }
