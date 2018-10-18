e4e83a7e1b8d9e51cf8811c62295d32e0a6a6349
Siyu Jiang <blackjackjiang@gmail.com>
2018-10-17 22:57:48 -0700

    Change to leverage creat-bootstrap default responsive navbar

--- src/components/YepsNavBar.tsx
+++ src/components/YepsNavBar.tsx
@@ -1,7 +1,6 @@
 import * as React from 'react';
 
-import { NavLink } from 'react-router-dom'
-import { Nav, Navbar } from 'react-bootstrap';
+import { NavItem, Nav, Navbar } from 'react-bootstrap';
 
 import './YepsNavBar.css';
 
@@ -9,26 +8,27 @@ export const YepsNavBar: React.StatelessComponent<{}> = () => {
     const logo = require('../images/logo.png');
 
     return (
-        <Navbar inverse collapseOnSelect>
+        <Navbar className="navBarRectangle" inverse collapseOnSelect>
             <Navbar.Header>
                 <Navbar.Brand>
                     <img className="navbar-icon" src={logo} alt="logo"/>
                 </Navbar.Brand>
+                <Navbar.Toggle />
             </Navbar.Header>
             <Navbar.Collapse>
                 <Nav pullRight>
-                    <NavLink className="font" to="#keynote">
+                    <NavItem eventKey={1} href="#">
                         Keynote
-                    </NavLink>
-                    <NavLink className="font" to="#speakers">
+                    </NavItem>
+                    <NavItem eventKey={2} href="#">
                         Speakers
-                    </NavLink>
-                    <NavLink className="font" to="#agenda">
+                    </NavItem>
+                    <NavItem eventKey={3} href="#">
                         Agenda
-                    </NavLink>
-                    <NavLink className="font" to="#about">
+                    </NavItem>
+                    <NavItem eventKey={4} href="#">
                         About
-                    </NavLink>
+                    </NavItem>
                 </Nav>
             </Navbar.Collapse>
         </Navbar>
