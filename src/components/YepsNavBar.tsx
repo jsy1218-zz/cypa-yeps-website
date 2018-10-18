62629b67a4f379be04fd60d9ccd8a2913016bf0a
Siyu Jiang <blackjackjiang@gmail.com>
2018-10-18 01:30:19 -0700

    Rough navbar with min asthetic

--- src/components/YepsNavBar.tsx
+++ src/components/YepsNavBar.tsx
@@ -1,38 +1,51 @@
 import * as React from 'react';
+import * as classnames from 'classnames/bind';
 
-import { Button } from 'react-bootstrap';
+import { Button, Grid, Row, Col } from 'react-bootstrap';
 import { NavItem, Nav, Navbar } from 'react-bootstrap';
+
+const cx = classnames.bind(require('./YepsNavBar.scss'));
 
 export const YepsNavBar: React.StatelessComponent<{}> = () => {
     const logo = require('../images/logo.png');
 
     return (
-        <Navbar className="navBarRectangle" collapseOnSelect>
-            <Navbar.Header>
-                <Navbar.Brand>
-                    <img className="navbar-icon" src={logo} alt="logo"/>
-                </Navbar.Brand>
-                <Navbar.Toggle />
-            </Navbar.Header>
-            <Navbar.Form pullLeft>
-                <Button type="submit">Language</Button>
-            </Navbar.Form>
-            <Navbar.Collapse>
-                <Nav pullRight>
-                    <NavItem eventKey={1} href="#">
-                        Keynote
-                    </NavItem>
-                    <NavItem eventKey={2} href="#">
-                        Speakers
-                    </NavItem>
-                    <NavItem eventKey={3} href="#">
-                        Agenda
-                    </NavItem>
-                    <NavItem eventKey={4} href="#">
-                        About
-                    </NavItem>
-                </Nav>
-            </Navbar.Collapse>
-        </Navbar>
+        <Grid fluid>
+            <Row>
+                <Navbar collapseOnSelect>
+                    <Col xs={2} md={4}>
+                        <Navbar.Header>
+                            <Navbar.Brand>
+                                <img className={cx('logo-wrapper')} src={logo} alt="logo"/>
+                            </Navbar.Brand>
+                        </Navbar.Header>
+                        <Navbar.Toggle/>
+                    </Col>
+                    <Col xs={3} md={4}>
+                        <Button type="submit">Language</Button>
+                    </Col>
+                    <Col xs={7} md={4}>
+                        <Nav pullRight>
+                            <Navbar.Collapse>
+                                <Nav>
+                                <NavItem eventKey={1} href="#">
+                                    Keynote
+                                </NavItem>
+                                <NavItem eventKey={2} href="#">
+                                    Speakers
+                                </NavItem>
+                                <NavItem eventKey={3} href="#">
+                                    Agenda
+                                </NavItem>
+                                <NavItem eventKey={4} href="#">
+                                    About
+                                </NavItem>
+                                </Nav>
+                            </Navbar.Collapse>
+                        </Nav>
+                    </Col>
+                </Navbar>
+            </Row>
+        </Grid>
     );
 }
