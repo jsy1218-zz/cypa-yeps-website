6e709b4dff1e9d9792cbfb34184c4f979c6ba180
Siyu Jiang <blackjackjiang@gmail.com>
2018-10-18 23:57:39 -0700

    Change the navbar to almost align with the responsive collapse example, but with button in the dropdown menu. Since it is hard to put next to the dropdown aligned

--- src/components/YepsNavBar.tsx
+++ src/components/YepsNavBar.tsx
@@ -1,7 +1,7 @@
 import * as React from 'react';
 import * as classnames from 'classnames/bind';
 
-import { Button, Grid, Row, Col } from 'react-bootstrap';
+import { Button } from 'react-bootstrap';
 import { NavItem, Nav, Navbar } from 'react-bootstrap';
 
 const cx = classnames.bind(require('./YepsNavBar.scss'));
@@ -10,42 +10,26 @@ export const YepsNavBar: React.StatelessComponent<{}> = () => {
     const logo = require('../images/logo.png');
 
     return (
-        <Grid fluid>
-            <Row>
-                <Navbar collapseOnSelect>
-                    <Col xs={2} md={4}>
-                        <Navbar.Header>
-                            <Navbar.Brand>
-                                <img className={cx('logo-wrapper')} src={logo} alt="logo"/>
-                            </Navbar.Brand>
-                        </Navbar.Header>
-                        <Navbar.Toggle/>
-                    </Col>
-                    <Col xs={3} md={4}>
-                        <Button type="submit">Language</Button>
-                    </Col>
-                    <Col xs={7} md={4}>
-                        <Nav pullRight>
-                            <Navbar.Collapse>
-                                <Nav>
-                                <NavItem eventKey={1} href="#">
-                                    Keynote
-                                </NavItem>
-                                <NavItem eventKey={2} href="#">
-                                    Speakers
-                                </NavItem>
-                                <NavItem eventKey={3} href="#">
-                                    Agenda
-                                </NavItem>
-                                <NavItem eventKey={4} href="#">
-                                    About
-                                </NavItem>
-                                </Nav>
-                            </Navbar.Collapse>
-                        </Nav>
-                    </Col>
-                </Navbar>
-            </Row>
-        </Grid>
+        <Navbar collapseOnSelect>
+        <Navbar.Header>
+            <Navbar.Brand>
+                <img className={cx('logo-wrapper')} src={logo} alt="logo"/>
+            </Navbar.Brand>
+            <Navbar.Toggle />
+        </Navbar.Header>
+        <Navbar.Collapse>
+            <Nav pullRight>
+            <NavItem eventKey={1} href="#">
+                <Button type="submit">Language</Button>
+            </NavItem>
+            <NavItem eventKey={2} href="#">
+                Link Right
+            </NavItem>
+            <NavItem eventKey={3} href="#">
+                Link Right
+            </NavItem>
+            </Nav>
+        </Navbar.Collapse>
+        </Navbar>
     );
 }
