f337a7b312b09a4d3a06d9e335ee323c1578eb61
jsy1218 <jsy1218@uw.edu>
2018-10-28 14:52:22 -0700

    Refactor speaker stickers into constant

--- src/components/Speakers.tsx
+++ src/components/Speakers.tsx
@@ -2,14 +2,16 @@ import * as React from 'react';
 import { Carousel } from 'react-bootstrap';
 import { Button, Grid, Row, Col } from 'react-bootstrap';
 
+import { SpeakersSticker } from '../constants/SpeakersSticker';
+
 import "./Speakers.css";
 
 export const Speakers: React.StatelessComponent<{}> = () => {
-    let speakersProps: { imgSource: any, name: string}[] = [
-        { imgSource: require('../images/Emily Shuka.png'), name: "Emily Shuka" }
-       ,{ imgSource: require('../images/Steve Stubbs 1.png'), name: "Steve Stubbs" }
-       ,{ imgSource: require('../images/Steve Stubbs 2.png'), name: "Steve Stubbs" }
-       ,{ imgSource: require('../images/Steve Stubbs 3.png'), name: "Steve Stubbs" }
+    let speakersProps: { imgSource: any, name: string, title: string, company: string }[] = [
+        { imgSource: require('../images/Emily Shuka.png'), name: "Emily Shuka", title: "Founder", company: "Apple" }
+       ,{ imgSource: require('../images/Steve Stubbs 1.png'), name: "Steve Stubbs", title: "Founder", company: "Apple" }
+       ,{ imgSource: require('../images/Steve Stubbs 2.png'), name: "Steve Stubbs", title: "Founder", company: "Apple" }
+       ,{ imgSource: require('../images/Steve Stubbs 3.png'), name: "Steve Stubbs", title: "Founder", company: "Apple" }
    ];
 
     return (
@@ -26,27 +28,28 @@ export const Speakers: React.StatelessComponent<{}> = () => {
                     <Carousel.Caption>
                         <h3>{speakersProps[0].name}</h3>
                     </Carousel.Caption>
-                    <div className="speakers-sticker-layout">
-                        <span className="speakers-sticker-font">Founder | Apple</span>
-                    </div>
+                    <SpeakersSticker title={speakersProps[0].title} company={speakersProps[0].company} />
                 </Carousel.Item> 
                 <Carousel.Item>
                     <img className="speakers-img" src={speakersProps[1].imgSource} />
                     <Carousel.Caption>
                         <h3>{speakersProps[1].name}</h3>
                     </Carousel.Caption>
+                    <SpeakersSticker title={speakersProps[1].title} company={speakersProps[1].company} />
                 </Carousel.Item>
                 <Carousel.Item>
                     <img className="speakers-img" src={speakersProps[2].imgSource} />
                     <Carousel.Caption>
                         <h3>{speakersProps[2].name}</h3>
                     </Carousel.Caption>
+                    <SpeakersSticker title={speakersProps[2].title} company={speakersProps[2].company} />
                 </Carousel.Item>
                 <Carousel.Item>
                     <img className="speakers-img" src={speakersProps[3].imgSource} />
                     <Carousel.Caption>
                         <h3>{speakersProps[3].name}</h3>
                     </Carousel.Caption>
+                    <SpeakersSticker title={speakersProps[3].title} company={speakersProps[3].company} />
                 </Carousel.Item>
             </Carousel>
 
---
+++ src/constants/SpeakersSticker.tsx
@@ -0,0 +1,14 @@
+import * as React from 'react';
+
+import '../components/Speakers.css';
+
+export interface Props {
+    title: string;
+    company: string;
+}
+
+export const SpeakersSticker = ({ title, company }: Props) => (
+    <div className="speakers-sticker-layout">
+        <span className="speakers-sticker-font">{title} | {company}</span>
+    </div>
+);
