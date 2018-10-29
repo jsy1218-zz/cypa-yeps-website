83da9b8d4256d41d92aab2602d5410bc346b304e
jsy1218 <jsy1218@uw.edu>
2018-10-28 23:53:00 -0700

    Use the curernt instance of top navbar for checking whether it is the collapse-expand context instead of checking the device source, for better accuracy. If the expand is undefined, then the nav items must not be collapsed, a golden source of truth

--- src/components/YepsNavBar.tsx
+++ src/components/YepsNavBar.tsx
@@ -2,8 +2,7 @@ import * as React from 'react';
 
 import { Button } from 'react-bootstrap';
 import { NavItem, Nav, Navbar } from 'react-bootstrap';
-import { isMobile, isBrowser } from 'react-device-detect'; 
-
+ 
 import { KEYNOTE_ROUTE, SPEAKERS_ROUTE } from '../constants/AppRouterConstants';
 
 import './YepsNavBar.css';
@@ -12,6 +11,8 @@ interface Props { }
 interface State { isExpanded: boolean; }
 
 export default class YepsNavBar extends React.Component<Props, State> {
+    private rootNavbar = React.createRef<Navbar>();
+
     constructor(props: Props) {
         super(props);
         this.state = { isExpanded: false };
@@ -22,8 +23,8 @@ export default class YepsNavBar extends React.Component<Props, State> {
     }
     
     renderTopButton = () => {
-        if (isMobile) {
-            return  <Navbar.Form pullLeft className={'test-language-button'}>
+        if (!this.isNavItemsCollapsed()) {
+            return  <Navbar.Form pullRight className={'test-language-button'}>
                         <Button className={'language-button'} type="submit">Language</Button>
                     </Navbar.Form>
         }
@@ -32,13 +33,17 @@ export default class YepsNavBar extends React.Component<Props, State> {
     }
 
     renderBottomButton = () => {
-        if (isBrowser) {
+        if (this.isNavItemsCollapsed()) {
             return  <Navbar.Form pullLeft className={'test-language-button-in-collapse'}>
                         <Button className={'language-button'} type="submit">Language</Button>
                     </Navbar.Form>
         }
 
         return '';
+    }
+
+    private isNavItemsCollapsed(): boolean {
+        return this.rootNavbar.current == null || this.rootNavbar.current.props.expanded != undefined;
     }
 
     render() {
@@ -48,7 +53,7 @@ export default class YepsNavBar extends React.Component<Props, State> {
         return (
             
             <div className={'navbar-default'}> 
-                <Navbar collapseOnSelect  id="top-navbar">
+                <Navbar collapseOnSelect  id="top-navbar" ref={this.rootNavbar}>
                     <Navbar.Header>
                         <Navbar.Brand>
                             <img className={'logo-wrapper'} src={logo} alt="logo"/>
