e34bc38f107fb86e6d154dddd5ba3785f074322b
Siyu Jiang <blackjackjiang@gmail.com>
2019-01-10 00:58:18 -0800

    Updates

---
+++ precache-manifest.c6353de2757a45ad8929b9e4817c75c0.js
@@ -0,0 +1,86 @@
+self.__precacheManifest = [
+  {
+    "revision": "31ff7cd8d8b414da8bbf70e4e8233946",
+    "url": "/cypa-yeps-website/static/media/wave.31ff7cd8.png"
+  },
+  {
+    "revision": "1a6ac831e8d3916928a2",
+    "url": "/cypa-yeps-website/static/css/main.a8727c10.chunk.css"
+  },
+  {
+    "revision": "e69585c8f46f928592b09fe40011762f",
+    "url": "/cypa-yeps-website/static/media/instagram.e69585c8.png"
+  },
+  {
+    "revision": "908926c6132c5e7bf31e",
+    "url": "/cypa-yeps-website/static/js/1.908926c6.chunk.js"
+  },
+  {
+    "revision": "643305c4de5ff317a8cf",
+    "url": "/cypa-yeps-website/static/js/runtime~main.643305c4.js"
+  },
+  {
+    "revision": "c1bf42e689cdad17a498ce5ccae2861a",
+    "url": "/cypa-yeps-website/static/media/logo.c1bf42e6.png"
+  },
+  {
+    "revision": "a94fd353ee345c4ca53ffe0a9764ddc0",
+    "url": "/cypa-yeps-website/static/media/Emily Shuka.a94fd353.png"
+  },
+  {
+    "revision": "93d0ce9ee5e950be4c1e3831773e9009",
+    "url": "/cypa-yeps-website/static/media/Steve Stubbs 1.93d0ce9e.png"
+  },
+  {
+    "revision": "1f6f6eb49c53a0a7a876ce76473e7e7f",
+    "url": "/cypa-yeps-website/static/media/Steve Stubbs 2.1f6f6eb4.png"
+  },
+  {
+    "revision": "0ec7780e44c2854ab4c3ce1d2e48b808",
+    "url": "/cypa-yeps-website/static/media/Steve Stubbs 3.0ec7780e.png"
+  },
+  {
+    "revision": "1a6ac831e8d3916928a2",
+    "url": "/cypa-yeps-website/static/js/main.1a6ac831.chunk.js"
+  },
+  {
+    "revision": "37c86acf5a64428fece18ad97d960791",
+    "url": "/cypa-yeps-website/static/media/discussion.37c86acf.png"
+  },
+  {
+    "revision": "c083a55f56c239376ad7314477f59f2f",
+    "url": "/cypa-yeps-website/static/media/wave.c083a55f.svg"
+  },
+  {
+    "revision": "8427b2ba5d024ceb41c03bde9f928f59",
+    "url": "/cypa-yeps-website/static/media/valley.8427b2ba.png"
+  },
+  {
+    "revision": "763dfde2d1cfb25e341f5e49effb0932",
+    "url": "/cypa-yeps-website/static/media/seattle.763dfde2.png"
+  },
+  {
+    "revision": "947c9a0f3961138a53f30d53b54610e3",
+    "url": "/cypa-yeps-website/static/media/washington.947c9a0f.png"
+  },
+  {
+    "revision": "f8e433bd5e9b31a2fc298ecc000f71b6",
+    "url": "/cypa-yeps-website/static/media/cypa.f8e433bd.png"
+  },
+  {
+    "revision": "90bb90121702c03b6da051c2a7f5bfe1",
+    "url": "/cypa-yeps-website/static/media/wechat.90bb9012.png"
+  },
+  {
+    "revision": "75790097bcf37849deee700e08814880",
+    "url": "/cypa-yeps-website/static/media/linkedin.75790097.png"
+  },
+  {
+    "revision": "908926c6132c5e7bf31e",
+    "url": "/cypa-yeps-website/static/css/1.5912fd36.chunk.css"
+  },
+  {
+    "revision": "d7da3297529b8ac0dea9805bee7fc14a",
+    "url": "/cypa-yeps-website/index.html"
+  }
+];
