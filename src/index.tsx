7d7109f67516439994e909722a11280af1a13f13
WangNini <Nini@WangNinis-MacBook-Pro.local>
2019-01-29 10:31:21 -0800

    split router

--- src/index.tsx
+++ src/index.tsx
@@ -2,10 +2,23 @@ import * as React from 'react';
 import * as ReactDOM from 'react-dom';
 
 import { AppRouter } from './AppRouter';
+import {DesktopAppRouter} from './DesktopAppRouter';
 
 import './index.css';
 
-ReactDOM.render(
-  <AppRouter />,
-  document.getElementById('root') as HTMLElement
-);
+function isDesktopDevice() {
+  return (typeof window.orientation == "undefined") && (navigator.userAgent.indexOf('IEMobile') == -1);
+};
+
+if (isDesktopDevice())
+{
+  ReactDOM.render(
+    <DesktopAppRouter />,
+    document.getElementById('root') as HTMLElement
+  );
+} else {
+  ReactDOM.render(
+    <AppRouter />,
+    document.getElementById('root') as HTMLElement
+  );
+}
