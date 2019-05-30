4d3fcfa36eb163c4b7286f29f8b12c77b0978b0e
Wilsonjeff <1093989171@qq.com>
2019-05-30 15:24:29 +0800

    speakers and  logo changes

--- src/components/Agenda.tsx
+++ src/components/Agenda.tsx
@@ -24,37 +24,49 @@ export const Agenda: React.StatelessComponent<{}> = () => {
     
                     <VerticalTimelineElement
                         className="vertical-timeline-element--work"
-                        date="7:30 - 8:00 AM"
+                        date="1:00 - 1:30 PM"
                         icon={<svg xmlns="http://www.w3.org/2000/svg" version="1.1">
                                 <circle cx="12" cy="13" r="9" stroke="#ffa719" stroke-width="2" fill="white" />
                                 <circle cx="12" cy="13" r="4" stroke="#ffa719" stroke-width="2" fill="#ffa719" />
                             </svg>} 
                     >
-                        <span className="vertical-timeline-element-title">Registration Opens</span>
+                        <span className="vertical-timeline-element-title">Sign In</span>
                     </VerticalTimelineElement>
                     <VerticalTimelineElement
                         className="vertical-timeline-element--work"
-                        date="8:00 - 8:30 AM"
+                        date="1:35 - 2:45 PM"
                         icon={<svg xmlns="http://www.w3.org/2000/svg" version="1.1">
                                 <circle cx="12" cy="13" r="9" stroke="#ffa719" stroke-width="2" fill="white" />
                                 <circle cx="12" cy="13" r="4" stroke="#ffa719" stroke-width="2" fill="#ffa719" />
                             </svg>}                 
                     >
-                        <span className="vertical-timeline-element-title">Welcome</span>
+                        <span className="vertical-timeline-element-title">Main Forum</span>
+                        <p className="agenda-paragraph">
+                        The host to introduce the opening, and The chairman delivered the opening speech. The main venue will preside by Zhenggang Yan. With the other three guests, Ruilin Li ( former mayor of  Bellevue), Nanyan Li (vice President of amazon AWS), Zhiling Zheng (vice President of engineering at GoDaddy). They will have in-depth discussions on education, workplace, entrepreneurship and public welfare.
+                        </p>
                     </VerticalTimelineElement>
                     <VerticalTimelineElement
                         className="vertical-timeline-element--work"
-                        date="8:30 - 9:30 AM"
+                        date="2:45 - 2:55 PM"
                         icon={<svg xmlns="http://www.w3.org/2000/svg" version="1.1">
                                 <circle cx="12" cy="13" r="9" stroke="#ffa719" stroke-width="2" fill="white" />
                                 <circle cx="12" cy="13" r="4" stroke="#ffa719" stroke-width="2" fill="#ffa719" />
                             </svg>}     
                     >
-                        <span className="vertical-timeline-element-title">Keynote: Career</span>
-                        <p className="agenda-paragraph">
-                        Barcoding's commitment to our customers is that we provide them with the ability to be more efficient, 
-                        accurate, and connected. This year we will host two content tracks to help you do just that.
-                        </p>
+                    <span className="vertical-timeline-element-title">Society Awards</span>
+                    <p className="agenda-paragraph">
+                    Presentation of Seattle excellent Society. The main purpose is to let the student community understand and affirm each other with the outside world.
+                    </p>
+                    </VerticalTimelineElement>
+                    <VerticalTimelineElement
+                        className="vertical-timeline-element--work"
+                        date="2:55 - 3:00 PM"
+                        icon={<svg xmlns="http://www.w3.org/2000/svg" version="1.1">
+                                <circle cx="12" cy="13" r="9" stroke="#ffa719" stroke-width="2" fill="white" />
+                                <circle cx="12" cy="13" r="4" stroke="#ffa719" stroke-width="2" fill="#ffa719" />
+                            </svg>}     
+                    >
+                    <span className="vertical-timeline-element-title">The Break</span>
                     </VerticalTimelineElement>
     
                     <div className="vertical-timeline-element-content agenda-meme">
@@ -63,26 +75,40 @@ export const Agenda: React.StatelessComponent<{}> = () => {
     
                     <VerticalTimelineElement
                         className="vertical-timeline-element--work"
-                        date="12:00 - 1:00 PM"
+                        date="3:00 - 4:10 PM"
                         icon={<svg xmlns="http://www.w3.org/2000/svg" version="1.1">
                                 <circle cx="12" cy="13" r="9" stroke="#ffa719" stroke-width="2" fill="white" />
                                 <circle cx="12" cy="13" r="4" stroke="#ffa719" stroke-width="2" fill="#ffa719" />
                             </svg>}     
                     >
-                        <span className="vertical-timeline-element-title">Lunch</span>
+                        <span className="vertical-timeline-element-title">Career Forum</span>     
+                    <p className="agenda-paragraph">
+                        Michielle Zhou keynote speech - tips for career growth <br/>
+                        Michielle Zhou, Yikun Zhao, Bo Li, Yu Fu as Panelist
+                    </p>
+                    </VerticalTimelineElement>
+                    <VerticalTimelineElement
+                        className="vertical-timeline-element--work"
+                        date="4:10 - 4:15 PM"
+                        icon={<svg xmlns="http://www.w3.org/2000/svg" version="1.1">
+                                <circle cx="12" cy="13" r="9" stroke="#ffa719" stroke-width="2" fill="white" />
+                                <circle cx="12" cy="13" r="4" stroke="#ffa719" stroke-width="2" fill="#ffa719" />
+                            </svg>}     
+                    >
+                    <span className="vertical-timeline-element-title">The Break</span>
                     </VerticalTimelineElement>
                     <VerticalTimelineElement
                         className="vertical-timeline-element--education"
-                        date="1:00 - 2:00 PM"
+                        date="4:15 - 6:30 PM"
                         icon={<svg xmlns="http://www.w3.org/2000/svg" version="1.1">
                                 <circle cx="12" cy="13" r="9" stroke="#ffa719" stroke-width="2" fill="white" />
                                 <circle cx="12" cy="13" r="4" stroke="#ffa719" stroke-width="2" fill="#ffa719" />
                             </svg>}     
-                    >
-                        <span className="vertical-timeline-element-title">Breakout Sessions</span>
+                    > 
+                        <span className="vertical-timeline-element-title">Entrepreneur Forum</span>
                         <p className="agenda-paragraph">
-                        Barcoding's commitment to our customers is that we provide them with the ability to be more efficient, 
-                        accurate, and connected.
+                        Seesion1: Zhihong Hong (founder and CEO of ArcBlock block), Chao Zhang (chief technology officer and co-founder of Ubi Interactive Inc), Stephanie Kim and Christopher Brown. They introduce the development prospects of the relevant fields and share their entrepreneurial stories.<br/>
+                        Seesion2: Nan Zi (co-founder of UniWill Ventures), Zhou Chen ( partner of OnePiece), Ming Zhu (founder and CEO of Yirental), Yuyang Fan (chief representative of YCE North America). They share the information related to youth entrepreneurship with you from the perspective of investors and incubators.
                         </p>
                     </VerticalTimelineElement>
     
@@ -92,13 +118,13 @@ export const Agenda: React.StatelessComponent<{}> = () => {
     
                     <VerticalTimelineElement
                         className="vertical-timeline-element--education"
-                        date="4:30 - 5:00 PM"
+                        date="6:30 - 7:00 PM"
                         icon={<svg xmlns="http://www.w3.org/2000/svg" version="1.1">
                                 <circle cx="12" cy="13" r="9" stroke="#ffa719" stroke-width="2" fill="white" />
                                 <circle cx="12" cy="13" r="4" stroke="#ffa719" stroke-width="2" fill="#ffa719" />
                             </svg>}     
                     >
-                        <span className="vertical-timeline-element-title">Closing Remarks</span>
+                        <span className="vertical-timeline-element-title">Networking</span>
                     </VerticalTimelineElement>
                 </VerticalTimeline>
             </Grid>
@@ -124,8 +150,8 @@ export const Agenda: React.StatelessComponent<{}> = () => {
                             <circle cx="12" cy="13" r="4" stroke="#ffa719" stroke-width="2" fill="#ffa719" />
                         </svg>} 
                 >
-                    <h3 className="vertical-timeline-element-title">7:30 - 8:00 AM</h3>
-                    <h3 className="vertical-timeline-element-title">Registration Opens</h3>
+                    <h3 className="vertical-timeline-element-title">1:00 - 1:30 PM</h3>
+                    <h3 className="vertical-timeline-element-title">Sign In</h3>
                 </VerticalTimelineElement>
                 <VerticalTimelineElement
                     className="vertical-timeline-element--work"
@@ -133,8 +159,23 @@ export const Agenda: React.StatelessComponent<{}> = () => {
                             <circle cx="12" cy="13" r="9" stroke="#ffa719" stroke-width="2" fill="white" />
                             <circle cx="12" cy="13" r="4" stroke="#ffa719" stroke-width="2" fill="#ffa719" />
                         </svg>}                 >
-                    <h3 className="vertical-timeline-element-title">8:00 - 8:30 AM</h3>
-                    <h3 className="vertical-timeline-element-title">Welcome</h3>
+                    <h3 className="vertical-timeline-element-title">1:35 - 2:45 PM</h3>
+                    <h3 className="vertical-timeline-element-title">Main Forum</h3>
+                    <p className="agenda-paragraph">
+                    The host to introduce the opening, and The chairman delivered the opening speech. The main venue will preside by Zhenggang Yan. With the other three guests, Ruilin Li ( former mayor of  Bellevue), Nanyan Li (vice President of amazon AWS), Zhiling Zheng (vice President of engineering at GoDaddy). They will have in-depth discussions on education, workplace, entrepreneurship and public welfare.
+                    </p>
+                </VerticalTimelineElement>
+                <VerticalTimelineElement
+                    className="vertical-timeline-element--work"
+                    icon={<svg xmlns="http://www.w3.org/2000/svg" version="1.1">
+                            <circle cx="12" cy="13" r="9" stroke="#ffa719" stroke-width="2" fill="white" />
+                            <circle cx="12" cy="13" r="4" stroke="#ffa719" stroke-width="2" fill="#ffa719" />
+                        </svg>}                 >
+                    <h3 className="vertical-timeline-element-title">2:45 - 2:55 PM</h3>
+                    <h3 className="vertical-timeline-element-title">Society Awards</h3>
+                    <p className="agenda-paragraph">
+                    Presentation of Seattle excellent Society. The main purpose is to let the student community understand and affirm each other with the outside world.
+                    </p>
                 </VerticalTimelineElement>
                 <VerticalTimelineElement
                     className="vertical-timeline-element--work"
@@ -143,12 +184,8 @@ export const Agenda: React.StatelessComponent<{}> = () => {
                             <circle cx="12" cy="13" r="4" stroke="#ffa719" stroke-width="2" fill="#ffa719" />
                         </svg>}     
                 >
-                    <h3 className="vertical-timeline-element-title">8:30 - 9:30 AM</h3>
-                    <h3 className="vertical-timeline-element-title">Keynote: Career</h3>
-                    <p className="agenda-paragraph">
-                    Barcoding's commitment to our customers is that we provide them with the ability to be more efficient, 
-                    accurate, and connected. This year we will host two content tracks to help you do just that.
-                    </p>
+                    <h3 className="vertical-timeline-element-title">2:55 - 3:00 PM</h3>
+                    <h3 className="vertical-timeline-element-title">The Break</h3>
                 </VerticalTimelineElement>
 
                 <div className="vertical-timeline-element-content agenda-meme">
@@ -162,8 +199,12 @@ export const Agenda: React.StatelessComponent<{}> = () => {
                             <circle cx="12" cy="13" r="4" stroke="#ffa719" stroke-width="2" fill="#ffa719" />
                         </svg>}     
                 >
-                    <h3 className="vertical-timeline-element-title">12:00 - 1:00 PM</h3>
-                    <h3 className="vertical-timeline-element-title">Lunch</h3>
+                    <h3 className="vertical-timeline-element-title">3:00 - 4:10 PM</h3>
+                    <h3 className="vertical-timeline-element-title">Career Forum</h3>
+                    <p className="agenda-paragraph">
+                        Michielle Zhou keynote speech - tips for career growth <br/>
+                        Michielle Zhou, Yikun Zhao, Bo Li, Yu Fu as Panelist
+                    </p>
                 </VerticalTimelineElement>
                 <VerticalTimelineElement
                     className="vertical-timeline-element--education"
@@ -172,11 +213,21 @@ export const Agenda: React.StatelessComponent<{}> = () => {
                             <circle cx="12" cy="13" r="4" stroke="#ffa719" stroke-width="2" fill="#ffa719" />
                         </svg>}     
                 >
-                    <h3 className="vertical-timeline-element-title">1:00 - 2:00 PM</h3>
-                    <h3 className="vertical-timeline-element-title">Breakout Sessions</h3>
+                    <h3 className="vertical-timeline-element-title">4:10 - 4:15 PM</h3>
+                    <h3 className="vertical-timeline-element-title">The Break</h3>
+                </VerticalTimelineElement>
+                <VerticalTimelineElement
+                    className="vertical-timeline-element--work"
+                    icon={<svg xmlns="http://www.w3.org/2000/svg" version="1.1">
+                            <circle cx="12" cy="13" r="9" stroke="#ffa719" stroke-width="2" fill="white" />
+                            <circle cx="12" cy="13" r="4" stroke="#ffa719" stroke-width="2" fill="#ffa719" />
+                        </svg>}     
+                >
+                    <h3 className="vertical-timeline-element-title">4:15 - 6:30 PM</h3>
+                    <h3 className="vertical-timeline-element-title">Entrepreneur Forum</h3>
                     <p className="agenda-paragraph">
-                    Barcoding's commitment to our customers is that we provide them with the ability to be more efficient, 
-                    accurate, and connected.
+                    Seesion1: Zhihong Hong (founder and CEO of ArcBlock block), Chao Zhang (chief technology officer and co-founder of Ubi Interactive Inc), Stephanie Kim and Christopher Brown. They introduce the development prospects of the relevant fields and share their entrepreneurial stories.<br/>
+                    Seesion2: Nan Zi (co-founder of UniWill Ventures), Zhou Chen ( partner of OnePiece), Ming Zhu (founder and CEO of Yirental), Yuyang Fan (chief representative of YCE North America). They share the information related to youth entrepreneurship with you from the perspective of investors and incubators.
                     </p>
                 </VerticalTimelineElement>
 
@@ -191,8 +242,8 @@ export const Agenda: React.StatelessComponent<{}> = () => {
                             <circle cx="12" cy="13" r="4" stroke="#ffa719" stroke-width="2" fill="#ffa719" />
                         </svg>}     
                 >
-                    <h3 className="vertical-timeline-element-title">4:30 - 5:00 PM</h3>
-                    <h3 className="vertical-timeline-element-title">Closing Remarks</h3>
+                    <h3 className="vertical-timeline-element-title">6:30 - 7:00 PM</h3>
+                    <h3 className="vertical-timeline-element-title">Networking</h3>
                 </VerticalTimelineElement>
             </VerticalTimeline>
 
